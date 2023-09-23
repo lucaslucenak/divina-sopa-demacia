3c20b0e1e94d0119e713ad35981575be4ec3a8c2
Lucas Lucena <lucas.lucenak@gmail.com>
2023-09-23 14:47:17 -0300

    Add controllers

--- src/main/java/com/lucaslucenak/Demacia/controllers/ClientAccountRequestController.java
+++ src/main/java/com/lucaslucenak/Demacia/controllers/ClientAccountRequestController.java
@@ -1,4 +1,23 @@
 package com.lucaslucenak.Demacia.controllers;
 
+import com.amazonaws.services.sqs.model.SendMessageResult;
+import com.lucaslucenak.Demacia.services.ClientAccountRequestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/client-account-request")
 public class ClientAccountRequestController {
+
+    @Autowired
+    ClientAccountRequestService clientAccountRequestService;
+
+    @PostMapping(value = "/save-clientAccount")
+    public ResponseEntity<SendMessageResult> requestClientAccount(@RequestBody String body) {
+        return ResponseEntity.ok().body(clientAccountRequestService.requestClientAccount(body));
+    }
 }
--- src/main/java/com/lucaslucenak/Demacia/controllers/SoupRequestController.java
+++ src/main/java/com/lucaslucenak/Demacia/controllers/SoupRequestController.java
@@ -1,4 +1,23 @@
 package com.lucaslucenak.Demacia.controllers;
 
+import com.amazonaws.services.sqs.model.SendMessageResult;
+import com.lucaslucenak.Demacia.services.SoupRequestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/soup-request")
 public class SoupRequestController {
+
+    @Autowired
+    SoupRequestService soupRequestService;
+
+    @PostMapping(value = "/save-soup")
+    public ResponseEntity<SendMessageResult> requestSoup(@RequestBody String body) {
+        return ResponseEntity.ok().body(soupRequestService.requestSoup(body));
+    }
 }
