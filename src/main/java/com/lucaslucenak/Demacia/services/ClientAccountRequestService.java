cdf1b38f08e0b87018fa5ef8ef01284b076e544a
Lucas Lucena <lucas.lucenak@gmail.com>
2023-09-23 14:39:23 -0300

    Add services

--- src/main/java/com/lucaslucenak/Demacia/services/ClientAccountRequestService.java
+++ src/main/java/com/lucaslucenak/Demacia/services/ClientAccountRequestService.java
@@ -1,4 +1,42 @@
 package com.lucaslucenak.Demacia.services;
 
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+import com.amazonaws.services.sqs.model.SendMessageResult;
+import com.lucaslucenak.Demacia.models.ClientAccountRequestModel;
+import com.lucaslucenak.Demacia.repositories.ClientAccountRequestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ClientAccountRequestService {
+
+    @Autowired
+    private AmazonSQS amazonSQS;
+    @Autowired
+    private ClientAccountRequestRepository clientAccountRequestRepository;
+
+    @Value("${aws.sqs.client-account.queue-url}")
+    private String queueUrl;
+
+    public SendMessageResult requestClientAccount(Object body) {
+        SendMessageRequest request = new SendMessageRequest()
+                .withQueueUrl(queueUrl)
+                .withMessageGroupId("save-client-account")
+                .withMessageBody(body.toString());
+
+        SendMessageResult sendMessageResult = amazonSQS.sendMessage(request);
+
+        ClientAccountRequestModel clientAccountRequestModel = new ClientAccountRequestModel();
+        clientAccountRequestModel.setRequestId(sendMessageResult.getSdkResponseMetadata().getRequestId());
+        clientAccountRequestModel.setMessageId(sendMessageResult.getMessageId());
+        clientAccountRequestModel.setBody(body.toString());
+        clientAccountRequestModel.setHttpStatus(sendMessageResult.getSdkHttpMetadata().getHttpStatusCode());
+        clientAccountRequestModel.setMd5OfMessageBody(sendMessageResult.getMD5OfMessageBody());
+
+        clientAccountRequestRepository.save(clientAccountRequestModel);
+
+        return sendMessageResult;
+    }
 }
--- src/main/java/com/lucaslucenak/Demacia/services/DrinkRequestService.java
+++ src/main/java/com/lucaslucenak/Demacia/services/DrinkRequestService.java
@@ -1,4 +1,42 @@
 package com.lucaslucenak.Demacia.services;
 
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+import com.amazonaws.services.sqs.model.SendMessageResult;
+import com.lucaslucenak.Demacia.models.DrinkRequestModel;
+import com.lucaslucenak.Demacia.repositories.DrinkRequestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
 public class DrinkRequestService {
+
+    @Autowired
+    private AmazonSQS amazonSQS;
+    @Autowired
+    private DrinkRequestRepository drinkRequestRepository;
+
+    @Value("${aws.sqs.drink.queue-url}")
+    private String queueUrl;
+
+    public SendMessageResult requestDrink(Object body) {
+        SendMessageRequest request = new SendMessageRequest()
+                .withQueueUrl(queueUrl)
+                .withMessageGroupId("save-drink")
+                .withMessageBody(body.toString());
+
+        SendMessageResult sendMessageResult = amazonSQS.sendMessage(request);
+
+        DrinkRequestModel drinkRequestModel = new DrinkRequestModel();
+        drinkRequestModel.setRequestId(sendMessageResult.getSdkResponseMetadata().getRequestId());
+        drinkRequestModel.setMessageId(sendMessageResult.getMessageId());
+        drinkRequestModel.setBody(body.toString());
+        drinkRequestModel.setHttpStatus(sendMessageResult.getSdkHttpMetadata().getHttpStatusCode());
+        drinkRequestModel.setMd5OfMessageBody(sendMessageResult.getMD5OfMessageBody());
+
+        drinkRequestRepository.save(drinkRequestModel);
+
+        return sendMessageResult;
+    }
 }
